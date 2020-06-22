574696413112b769051cf63b854d25e104529410
Santiago Salamanca <ssalamancal@psl.com.co>
2020-06-22 16:36:24 -0500

    Rename class and extract function

---
+++ GeneratePrimes/PrimeGenerator.cs
@@ -0,0 +1,103 @@
+﻿/// <remark>
+/// This class Generates prime numbers up to a user specified
+/// maximum. The algorithm used is the Sieve of Eratosthenes.
+///
+/// Eratosthenes of Cyrene, b. c. 276 BC, Cyrene, Libya --
+/// d. c. 194, Alexandria. The first man to calculate the
+/// circumference of the Earth. Also known for working on
+/// calendars with leap years and ran the library at
+/// Alexandria.
+///
+/// The algorithm is quite simple. Given an array of integers
+/// starting at 2. Cross out all multiples of 2. Find the
+/// next uncrossed integer, and cross out all of its multiples.
+/// Repeat until you have passed the square root of the
+/// maximum value.
+///
+/// Written by Robert C. Martin on 9 Dec 1999 in Java
+/// Translated to C# by Micah Martin on 12 Jan 2005.
+///</remark>
+
+using System;
+
+namespace GeneratePrimesRefactoring
+{
+    /// <summary>
+    /// author: Robert C. Martin
+    /// </summary>
+    public class PrimeGenerator
+    {
+        private static int s;
+        private static bool[] f;
+        private static int[] primes;
+
+        ///<summary>
+        /// Generates an array of prime numbers.
+        ///</summary>
+        ///
+        /// <param name="maxValue">The generation limit.</param>
+        public static int[] GeneratePrimeNumbers(int maxValue)
+        {
+            if (maxValue < 2) return new int[0];
+
+            InitializeSieve(maxValue);
+            Sieve();
+            LoadPrimes();
+
+            return primes;
+        }
+
+        private static void InitializeSieve(int maxValue)
+        {
+            // declarations
+            s = maxValue + 1; // size of array
+            f = new bool[s];
+            int i;
+
+            // initialize array to true.
+            for (i = 0; i < s; i++)
+                f[i] = true;
+            
+            // get rid of known non-primes
+            f[0] = f[1] = false;
+        }
+
+        private static void Sieve()
+        {
+            int i;
+            int j;
+            for (i = 2; i < Math.Sqrt(s) + 1; i++)
+            {
+                if (f[i]) // if i is uncrossed, cross its multiples.
+                {
+                    for (j = 2 * i; j < s; j += i)
+                        f[j] = false; // multiple is not prime
+                }
+            }
+        }
+
+        private static int[] LoadPrimes()
+        {
+            int i;
+            int j;
+
+            // how many primes are there?
+            int count = 0;
+            for (i = 0; i < s; i++)
+            {
+                if (f[i])
+                    count++; // bump count.
+            }
+
+            primes = new int[count];
+            // move the primes into the result
+            for (i = 0, j = 0; i < s; i++)
+            {
+                if (f[i]) // if prime
+                    primes[j++] = i;
+            }
+
+            return primes; // return the primes
+        }
+    }
+}
--- GeneratePrimesTest/GeneratePrimesTest.cs
+++ GeneratePrimesTest/GeneratePrimesTest.cs
@@ -9,18 +9,21 @@ namespace GeneratePrimesTest
         [Fact]
         public void TestPrimes()
         {
-            int[] nullArray = GeneratePrimes.GeneratePrimeNumbers(0);
+            int[] nullArray = PrimeGenerator.GeneratePrimeNumbers(0);
             Assert.Equal(nullArray.Length, 0);
-            int[] minArray = GeneratePrimes.GeneratePrimeNumbers(2);
+
+            int[] minArray = PrimeGenerator.GeneratePrimeNumbers(2);
             Assert.Equal(minArray.Length, 1);
             Assert.Equal(minArray[0], 2);
-            int[] threeArray = GeneratePrimes.GeneratePrimeNumbers(3);
+
+            int[] threeArray = PrimeGenerator.GeneratePrimeNumbers(3);
             Assert.Equal(threeArray.Length, 2);
             Assert.Equal(threeArray[0], 2);
             Assert.Equal(threeArray[1], 3);
-            int[] centArray = GeneratePrimes.GeneratePrimeNumbers(100);
+
+            int[] centArray = PrimeGenerator.GeneratePrimeNumbers(100);
             Assert.Equal(centArray.Length, 25);
             Assert.Equal(centArray[24], 97);
-        }
+        }
     }
 }
