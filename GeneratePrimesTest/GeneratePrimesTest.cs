94435bce1ddfed5bdc3291bf6d2ac8fc416e9577
Santiago Salamanca <ssalamancal@psl.com.co>
2020-06-22 13:27:45 -0500

    Initial commit

---
+++ GeneratePrimesTest/GeneratePrimesTest.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using GeneratePrimesRefactoring;
+
+namespace GeneratePrimesTest
+{
+    public class GeneratePrimesTest
+    {
+        [Fact]
+        public void TestPrimes()
+        {
+            int[] nullArray = GeneratePrimes.GeneratePrimeNumbers(0);
+            Assert.Equal(nullArray.Length, 0);
+            int[] minArray = GeneratePrimes.GeneratePrimeNumbers(2);
+            Assert.Equal(minArray.Length, 1);
+            Assert.Equal(minArray[0], 2);
+            int[] threeArray = GeneratePrimes.GeneratePrimeNumbers(3);
+            Assert.Equal(threeArray.Length, 2);
+            Assert.Equal(threeArray[0], 2);
+            Assert.Equal(threeArray[1], 3);
+            int[] centArray = GeneratePrimes.GeneratePrimeNumbers(100);
+            Assert.Equal(centArray.Length, 25);
+            Assert.Equal(centArray[24], 97);
+        }
+    }
+}
